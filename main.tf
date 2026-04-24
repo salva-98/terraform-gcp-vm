7cf6e877a0c6cc8784dc3dec0c92246cdb7d0652
Salvador Salazar <salva.s9801@gmail.com>
2026-04-24 08:50:29 +0000

    Update VM to e2-small

--- main.tf
+++ main.tf
@@ -5,25 +5,21 @@ provider "google" {
 }
 
 resource "google_compute_instance" "vm_instance" {
-  name         = "test-vm"
-  machine_type = "e2-micro"
+  name         = "hermes-personal-vm"
+  machine_type = "e2-small"
   zone         = "us-central1-a"
 
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-12"
+      size = 10
+      type = "pd-standard"
     }
   }
 
   network_interface {
     network = "default"
-
-    access_config {
-      # Ephemeral public IP
-    }
   }
-
-  tags = ["http-server"]
 
   metadata = {
     enable-oslogin = "TRUE"
