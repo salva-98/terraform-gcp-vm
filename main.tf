6e0438dd70403eaf042c651e90ad3bed6f1376b7
Salvador Salazar <salva.s9801@gmail.com>
2026-05-13 16:27:27 +0000

    Removing micro vm and adding new medium vm to transfer agent

--- main.tf
+++ main.tf
@@ -4,15 +4,15 @@ provider "google" {
   zone    = "us-central1-a"
 }
 
-resource "google_compute_instance" "vm_instance" {
-  name         = "test-vm"
-  machine_type = "e2-micro"
+resource "google_compute_instance" "hermes_vm_instance_main" {
+  name         = "hermes-vm"
+  machine_type = "e2-medium"
   zone         = "us-central1-a"
 
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-12"
-      size = 10
+      size = 30
       type = "pd-standard"
     }
   }
@@ -20,17 +20,12 @@ resource "google_compute_instance" "vm_instance" {
   network_interface {
     network = "default"
     access_config {
-      nat_ip       = "34.134.181.229"
-      network_tier = "PREMIUM"
     }
   }
 
   metadata = {
     enable-oslogin = "TRUE"
   }
-
-  tags = ["http-server"]
-  
 }
 
 resource "google_compute_instance" "hermes_vm_instance" {
@@ -41,7 +36,7 @@ resource "google_compute_instance" "hermes_vm_instance" {
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-12"
-      size = 10
+      size = 30
       type = "pd-standard"
     }
   }
